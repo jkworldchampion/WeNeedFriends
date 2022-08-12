594a446f9145392869de702ae17905256b01a0e1
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-08-12 19:22:22 +0900

    서버와 동기화
    서버로부터 비동기적으로 데이터를 받아와 화면을 출력한다

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -7,34 +7,24 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 
-const customers = [
-	{
-		'id' : 1,
-		'image' : 'https://placeimg.com/64/64/1',
-		'name' : '나동빈',
-		'birthday' : '980516',
-		'gender' : '남자',
-		'job' : '디자이너'
-	},
-	{
-		'id' : 2,
-		'image' : 'https://placeimg.com/64/64/2',
-		'name' : '박주환',
-		'birthday' : '000930',
-		'gender' : '남자',
-		'job' : '대학생'
-	},
-	{
-		'id' : 3,
-		//'image' : 'https://placeimg.com/64/64/3',
-		'image' : 'https://proxy.goorm.io/service/62e625b3963aff975fc24b76_d3uwCbw3tlLCJHYu880.run.goorm.io/9080/file/load/Ayoung.jpg?path=d29ya3NwYWNlJTJGVGFEYSUyRmNsb25lX2NvZGUlMkZtYW5hZ2VtZW50JTJGc3JjJTJGY29tcG9uZW50cyUyRkF5b3VuZy5qcGc=&docker_id=d3uwCbw3tlLCJHYu880&secure_session_id=OzerQejxcoY83HfWMzaH7nEh4pV1tjKS',
-		'name' : '이아영',
-		'birthday' : '010616',
-		'gender' : '여자',
-		'job' : '디자이너'	 
-}]
-
 class App extends Component { 
+	
+	state = {
+		customer: ""
+	}
+	
+	componentDidMount(){
+		this.callApi()
+			.then(res => this.setState({customers: res}))
+			.catch(err => console.log(err));
+	}
+	
+	callApi = async () => {
+		const response = await fetch('api/customers');
+		const body = await response.json();
+		return body;
+	}
+	
 	render() {  
 		return (  
 			<div>     
@@ -50,7 +40,7 @@ class App extends Component {
 						</TableRow>      
 					</TableHead> 
 					<TableBody>       
-						{customers.map(c => {     
+						{this.state.customers ? this.state.customers.map(c => {
 							return <Customer 
 											 key={c.id} 
 											 id={c.id} 
@@ -59,7 +49,7 @@ class App extends Component {
 											 birthday={c.birthday} 
 											 gender={c.gender} 
 											 job={c.job} />      
-						})}    
+						}) : "" }    
 					</TableBody> 
 				</Table>   
 			</div>   
