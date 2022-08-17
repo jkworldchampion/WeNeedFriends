d0a6273e695d725f6082670ab302f49d1c6c58d9
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-08-17 18:54:43 +0900

    '계획 추가' 추가
    근데 서버에서 값을 받아오질 못한다...

--- navytaxi/client/app.js
+++ navytaxi/client/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Timeplan from './components/Timeplan';
-import planAdd from './components/planAdd';
+import planAdd from './components/planAdd'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -11,6 +11,8 @@ import Paper from '@mui/material/Paper';
 import CircularProgress from '@mui/material/CircularProgress';
 import { styled } from '@mui/material/styles';
 import './App.css';
+
+import { post } from 'axios';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -36,6 +38,59 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 
 class App extends Component {
+	
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			departure: '',
+			arrival: '',
+			time: '',
+			number: '',
+			name: ''
+		}
+	}
+	
+	handelFormSubmit = (e) => {
+		e.preventDefault()
+		this.addPlan()
+			.then((response) => {
+				console.log(response.data);
+			})
+		this.setState({
+			departure: '',
+			arrival: '',
+			time: '',
+			number: '',
+			name: ''
+		})
+		window.location.reload();
+	}
+	
+	handleValueChange = (e) => {
+		let nextState = {};
+		nextState[e.target.name] = e.target.value;
+		this.setState(nextState);
+	}
+	
+	addPlan = () => {
+		const url = '/api/plans';
+		const formData = new FormData();
+		formData.append('departure', this.state.departure);
+		formData.append('arrival', this.state.arrival);
+		formData.append('time', this.state.time);
+		formData.append('number', this.state.number);
+		formData.append('name', this.state.name);
+		const config = {
+			headers: {
+				'content-type': 'multipart/form-data'
+			}
+		}
+		return post(url, formData, config);
+	}
+	
+	
+	
 	
 	state = {
 		plans: ""
@@ -90,6 +145,15 @@ class App extends Component {
 						</TableBody>
 					</Table>
 				</TableContainer>	
+				<form onSubmit={this.handelFormSubmit}>
+					<h1>계획 추가</h1>
+					출발지: <input type="text" name="departure" value={this.state.departure} onChange={this.handleValueChange} /><br/>
+					도착(목적)지: <input type="text" name="arrival" value={this.state.arrival} onChange={this.handleValueChange} /><br/> 
+					시간: <input type="text" name="time" value={this.state.time} onChange={this.handleValueChange} /><br/>
+					인원: <input type="text" name="number" value={this.state.number} onChange={this.handleValueChange} /><br/>
+					이름: <input type="text" name="name" value={this.state.name} onChange={this.handleValueChange} /><br/>
+					<button type="submit"> 추가하기</button>
+				</form>
 			</div>
 		);
 	}
