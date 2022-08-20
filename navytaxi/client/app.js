32dc71d13b4061becb3331a5f6f8cf80102dd96e
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-08-20 20:06:55 +0900

    add searching tool

--- navytaxi/client/app.js
+++ navytaxi/client/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Timeplan from './components/Timeplan';
 import PlanAdd from './components/PlanAdd';
-import SearchAppBar from './components/SearchAppBar';
+// import SearchAppBar from './components/SearchAppBar';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -10,7 +10,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import CircularProgress from '@mui/material/CircularProgress';
-import { styled } from '@mui/material/styles';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -36,6 +45,57 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
 
 class App extends Component {
 	
@@ -43,13 +103,15 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			plans: ''
+			plans: '',
+			searchKeyword: ''
 		}
 	}
 	
 	stateRefresh = () => {   // 상태를 다시 초기화 한 뒤, 고객 값을 불러옴
 		this.setState({
-			plans: ''
+			plans: '',
+			searchKeyword: ''
 		});
 		this.callApi()
 			.then(res => this.setState({plans: res}))
@@ -68,10 +130,57 @@ class App extends Component {
 		return body;
 	}
 	
+	handleValueChange = (e) => {
+		let nextState = {};
+		nextState[e.target.name] = e.target.value;
+		this.setState(nextState);
+	}
+	
 	render() {
+		const filteredComponents = (data) => {
+			data = data.filter((c) => {
+				return (c.departure.indexOf(this.state.searchKeyword) + c.arrival.indexOf(this.state.searchKeyword)) > -2;
+			});
+			return data.map((p) => {
+				return <Timeplan 
+											stateRefresh={this.stateRefresh}
+											key={p.id}
+											id={p.id}
+											departure={p.departure}
+											arrival={p.arrival}
+											time={p.time}
+											number={p.number}
+											name={p.name}
+										/> 
+			})
+		}
+		
 		return (
 			<div>
-				<SearchAppBar />
+				<Box sx={{ flexGrow: 1 }}>
+					<ThemeProvider theme={darkTheme}>
+						<AppBar position="static">
+							<Toolbar>
+								<IconButton size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }} >
+									<MenuIcon />
+								</IconButton>
+								<Typography variant="h6" noWrapcomponent="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }} >
+									NavyTaxi
+								</Typography>
+								<Search>
+									<SearchIconWrapper>
+										<SearchIcon />
+									</SearchIconWrapper>
+									<StyledInputBase placeholder="검색…" inputProps={{ 'aria-label': 'search' }} 
+										name="searchKeyword"
+										value={this.state.searchKeyword}
+										onChange={this.handleValueChange}
+										/>
+								</Search>
+							</Toolbar>
+						</AppBar>
+					</ThemeProvider>
+				</Box>
 				<div align="center">
 					<br/>
 					<PlanAdd stateRefresh={this.stateRefresh} />
@@ -91,20 +200,8 @@ class App extends Component {
 								</StyledTableRow>
 							</TableHead>
 							<TableBody>
-								{this.state.plans ? this.state.plans.map(p => {
-									return (
-										<Timeplan 
-											stateRefresh={this.stateRefresh}
-											key={p.id}
-											id={p.id}
-											departure={p.departure}
-											arrival={p.arrival}
-											time={p.time}
-											number={p.number}
-											name={p.name}
-										/>
-									);
-								}) : 
+								{this.state.plans ? 
+									filteredComponents(this.state.plans) :
 								<StyledTableRow>
 									<StyledTableCell colSpan="6" align="center">
 											<CircularProgress />
