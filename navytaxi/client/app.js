be0b481bbae6813f8818a137c25cc318ea690825
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-08-20 19:20:42 +0900

    add AppBar

--- navytaxi/client/app.js
+++ navytaxi/client/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Timeplan from './components/Timeplan';
 import PlanAdd from './components/PlanAdd';
+import SearchAppBar from './components/SearchAppBar';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -12,12 +13,12 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { styled } from '@mui/material/styles';
 import './App.css';
 
-import { post } from 'axios';
-
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+    //backgroundColor: theme.palette.common.black,
+		backgroundColor: '#FFEAD8',
+    //color: theme.palette.common.white,
+		color: '#000000',
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
@@ -26,7 +27,8 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
+		// backgroundColor: "FFEAD8"
+    backgroundColor: "theme.palette.action.hover",
   },
   // hide last border
   '&:last-child td, &:last-child th': {
@@ -35,61 +37,15 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 
-
-
 class App extends Component {
 	
 	
 	constructor(props) {
 		super(props);
 		this.state = {
-			plans: '',
-			departure: '',
-			arrival: '',
-			time: '',
-			number: '',
-			name: ''
+			plans: ''
 		}
 	}
-	
-	handelFormSubmit = (e) => {
-		e.preventDefault()
-		this.addPlan()
-			.then((response) => {
-				console.log(response.data);
-			})
-		this.setState({
-			departure: '',
-			arrival: '',
-			time: '',
-			number: '',
-			name: ''
-		})
-		window.location.reload();
-	}
-	
-	handleValueChange = (e) => {
-		let nextState = {};
-		nextState[e.target.name] = e.target.value;
-		this.setState(nextState);
-	}
-	
-	addPlan = () => {
-		const url = '/api/plans';
-		const formData = new FormData();
-		formData.append('departure', this.state.departure);
-		formData.append('arrival', this.state.arrival);
-		formData.append('time', this.state.time);
-		formData.append('number', this.state.number);
-		formData.append('name', this.state.name);
-		const config = {
-			headers: {
-				'content-type': 'multipart/form-data'
-			}
-		}
-		return post(url, formData, config);
-	}
-	
 	
 	stateRefresh = () => {   // 상태를 다시 초기화 한 뒤, 고객 값을 불러옴
 		this.setState({
@@ -111,46 +67,53 @@ class App extends Component {
 		const body = await response.json();
 		return body;
 	}
-	//
-	render() { //
-		return (//
+	
+	render() {
+		return (
 			<div>
-				<TableContainer component={Paper}>
-					<Table sx={{ minWidth: 700 }} aria-label="customized table">
-						<TableHead>
-							<StyledTableRow>
-								<StyledTableCell align="right">출발지</StyledTableCell>
-								<StyledTableCell align="right">도착지</StyledTableCell>
-								<StyledTableCell align="right">시간</StyledTableCell>
-								<StyledTableCell align="right">인원</StyledTableCell>
-								<StyledTableCell align="right">이름</StyledTableCell>
-								<StyledTableCell align="right">설정</StyledTableCell>
-							</StyledTableRow>
-						</TableHead>
-						<TableBody>
-							{this.state.plans ? this.state.plans.map(p => {
-								return (
-									<Timeplan 
-										stateRefresh={this.stateRefresh}
-										key={p.id}
-										id={p.id}
-										departure={p.departure}
-										arrival={p.arrival}
-										time={p.time}
-										number={p.number}
-										name={p.name}
-									/>
-								);
-							}) : 
-							<StyledTableRow>
-								<StyledTableCell colSpan="6" align="center">
-										<CircularProgress />
-								</StyledTableCell>
-							</StyledTableRow>}
-						</TableBody>
-					</Table>
-				</TableContainer>	
-				<PlanAdd stateRefresh={this.stateRefresh} />
+				<SearchAppBar />
+				<div align="center">
+					<br/>
+					<PlanAdd stateRefresh={this.stateRefresh} />
+					<br/>
+				</div>
+				<Paper>
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 500 }} aria-label="customized table">
+							<TableHead>
+								<StyledTableRow>
+									<StyledTableCell align="right">출발지</StyledTableCell>
+									<StyledTableCell align="right">도착지</StyledTableCell>
+									<StyledTableCell align="right">시간</StyledTableCell>
+									<StyledTableCell align="right">인원</StyledTableCell>
+									<StyledTableCell align="right">이름</StyledTableCell>
+									<StyledTableCell align="center">설정</StyledTableCell>
+								</StyledTableRow>
+							</TableHead>
+							<TableBody>
+								{this.state.plans ? this.state.plans.map(p => {
+									return (
+										<Timeplan 
+											stateRefresh={this.stateRefresh}
+											key={p.id}
+											id={p.id}
+											departure={p.departure}
+											arrival={p.arrival}
+											time={p.time}
+											number={p.number}
+											name={p.name}
+										/>
+									);
+								}) : 
+								<StyledTableRow>
+									<StyledTableCell colSpan="6" align="center">
+											<CircularProgress />
+									</StyledTableCell>
+								</StyledTableRow>}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
 			</div>
 		);
 	}
