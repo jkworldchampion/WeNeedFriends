b2b33144ef0c7ec8872feec5eff37a944f3bfc70
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-08-26 20:53:41 +0900

    add peopel.map

--- navytaxi/client/components/Timeplan.js
+++ navytaxi/client/components/Timeplan.js
@@ -4,6 +4,7 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import { styled } from '@mui/material/styles';
 
 import PlanDelete from './PlanDelete';
+import Withpeople from './Withpeople';
 import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
@@ -15,6 +16,7 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -37,66 +39,46 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function Row(props) {
-	const { row } = props;
-	const [open, setOpen] = React.useState(false);
-	
-	return (
-	<React.Fragment>
-		<StyledTableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-			<StyledTableCell>
-				<IconButton
-					aria-label="expand row"
-					size="small"
-					onClick={() => setOpen(!open)}
-				>
-					{open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-				</IconButton>
-			</StyledTableCell>
-			<StyledTableCell align="right">{this.props.departure}</StyledTableCell>
-			<StyledTableCell align="right">{this.props.arrival}</StyledTableCell>
-			<StyledTableCell align="right">{this.props.time}</StyledTableCell>
-			<StyledTableCell align="right">{this.props.number}</StyledTableCell>
-			<StyledTableCell align="right">{this.props.name}</StyledTableCell>
-			<StyledTableCell align="center"><PlanDelete stateRefresh={this.props.stateRefresh} id={this.props.id} /></StyledTableCell> 
-		</StyledTableRow>
-		<StyledTableRow>
-			<TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
-				<Collapse in={open} timeout="auto" unmountOnExit>
-					<Box sx={{ margin: 1 }}>
-						<Typography variant="h6" gutterBottom component="div">
-							Together
-						</Typography>
-					<Table size="small" aria-label="together">
-						<TableHead>
-							<TableRow>
-								<TableCell>이름</TableCell>
-								<TableCell>전화번호</TableCell>
-								<TableCell>출발지 도착시간</TableCell>
-							</TableRow>
-						</TableHead>	
-						<TableBody>
-							<TableRow>
-								<TableCell>이름이다</TableCell>
-								<TableCell>전화번호다</TableCell>
-								<TableCell>출발지 도착시간이다</TableCell>
-							</TableRow>
-						</TableBody>
-					</Table>
-					</Box>
-				</Collapse>
-			</TableCell>
-		</StyledTableRow>
-	</React.Fragment>
-	)
-}
+const pp = [
+	{
+		"id":1,
+		"name":"박주환",
+		"phonenumber":"010-7110-8128",
+		"time":"1805"
+	},
+	{
+		"id":1,
+		"name":"정의철",
+		"phonenumber":"010-4039-3970",
+		"time":"1810"
+	},
+	{
+		"id":2,
+		"name":"황동원",
+		"phonenumber":"010-2736-1234",
+		"time":"1500"
+	},
+	{
+		"id":2,
+		"name":"김김김",
+		"phonenumber":"010-1456-1234",
+		"time":"1500"
+	},
+	{
+		"id":2,
+		"name":"박박박",
+		"phonenumber":"010-1478-5534",
+		"time":"1500"
+	}
+]
 
 class Timeplan extends React.Component {
 	
 	constructor(props) {
 		super(props);
 		this.state = {
-			open: false
+			open: false,
+			people: ''
 		}
 	}
 	
@@ -106,6 +88,18 @@ class Timeplan extends React.Component {
 		});
 	}
 	
+	// 각 계획에 대한 사람들 정보 불러오기	
+	componentDidMountPeople() {
+		this.callpeople()
+			.then(res => this.setState({people: res}))
+			.catch(err => console.log(err));
+	}
+	
+	callpeople = async() => {
+		const response = await fetch('/api/planid');
+		const body = await response.json();
+		return body;
+	}
 	
 	render() {		
 		return (
@@ -143,6 +137,19 @@ class Timeplan extends React.Component {
 									</TableRow>
 								</TableHead>	
 								<TableBody>
+									<div>
+										{this.props.people.map((p, index) => {
+											let i = Math.floor(Math.random() * 1000+1)
+											return (
+												<Withpeople
+													key={i}
+													name={p.name}
+													phonenumber={p.phonenumber}
+													time={p.time}
+												/>
+											);
+										})}
+									</div>
 									<TableRow>
 										<TableCell>이름이다</TableCell>
 										<TableCell>전화번호다</TableCell>
